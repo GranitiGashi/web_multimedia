c20191fd38ac88667194ae81635bad4f69990163
Granit Gashi <granit.g4shii@gmail.com>
2024-05-30 00:35:29 +0200

    added some animations

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
+AOS.init();
+
 window.addEventListener('scroll', function() {
     const sections = document.querySelectorAll('.tool_section, .tool_section1, .tool_section3');
-    const scrollY = window.scrollY;
-    let colorIndex;
+    const scrollY = window.scrollY + 130;
 
-    // Determine the color index based on scroll position
-    if (scrollY < sections[0].offsetTop || scrollY > sections[2].offsetTop + sections[2].clientHeight) {
-        colorIndex = 0; // Use the first color when outside the range of the three sections
-    } else {
-        colorIndex = 1; // Use the second color when inside the range of the three sections
-    }
-
-    // Apply colors to sections
     sections.forEach((section, index) => {
-        if (index >= 0 && index <= 2) {
-            section.style.backgroundColor = getColor(colorIndex);
+        const offsetTop = section.offsetTop;
+        const offsetBottom = offsetTop + section.clientHeight;
+
+        if (scrollY >= offsetTop && scrollY < offsetBottom) {
+            section.style.backgroundColor = getColor(index);
         } else {
-            section.style.backgroundColor = getColor(index); // Different color for other sections
+            section.style.backgroundColor = getBaseColor();
         }
     });
 });
 
 function getColor(index) {
-    const colors = ['rgb(233, 121, 233)', '#da82a5', 'rgb(188, 92, 181)']; // Colors for the three sections
-    const otherColors = ['red', 'blue', 'green']; // Colors for other sections
-    return index < 3 ? colors[index] : otherColors[index - 3]; // Return appropriate color based on index
+    const colors = ['#E59A59', '#888870', '#712E1E'];
+    return colors[index % colors.length];
+}
+
+function getBaseColor() {
+    return '#FEBEB0';
 }
